1a5e1efa687d4029c1c799f9b464f7c2cbc14a35
seth@mojodna.net <seth@mojodna.net>
2007-11-27 20:21:19 +0000

    Rails 2.0.0 RC1
    git-svn-id: http://oauth.googlecode.com/svn/code/ruby/sample_oauth_provider@330 f7ae4463-c52f-0410-a7dc-93bad6e629e8

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,11 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
+  # load unpacked gems from vendor/gems
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end
+
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
